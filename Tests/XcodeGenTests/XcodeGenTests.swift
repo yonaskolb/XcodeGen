4ca94ea9392548039352736a78a51dd5d483fb5c
Yonas Kolb <yonaskolb@gmail.com>
2017-07-22 12:48:40 +0200

    add generation test

--- Tests/XcodeGenTests/XcodeGenTests.swift
+++ Tests/XcodeGenTests/XcodeGenTests.swift
@@ -1,14 +1,25 @@
 import XCTest
-@testable import XcodeGen
+import PathKit
+import XcodeGenKit
 
 class XcodeGenTests: XCTestCase {
-    func testExample() {
 
+    let fixturePath = Path(#file).parent().parent().parent() + "Fixtures"
 
+    func testGeneration() throws {
+        let specPath = fixturePath + "TestProject/spec.yml"
+        let spec = try Spec(path: specPath)
+        let lintedSpec = SpecLinter.lint(spec)
+        if lintedSpec.errors.isEmpty {
+            let generator = ProjectGenerator(spec: lintedSpec.spec, path: fixturePath + "TestProject/spec.xcodeproj")
+            let project = try generator.generate()
+            try project.write(override: true)
+        } else {
+            XCTFail("Spec has errors:\n\(lintedSpec.errors.map { $0.description}.joined(separator: "\n"))")
+        }
     }
 
-
     static var allTests = [
-        ("testExample", testExample),
-    ]
+        ("testGeneration", testGeneration),
+        ]
 }
