df1935875273ac146a83c7fad93713739b626c78
Yonas Kolb <yonaskolb@gmail.com>
2017-07-23 16:21:59 +0200

    add read test

--- Tests/XcodeGenKitTests/XcodeGenKitTests.swift
+++ Tests/XcodeGenKitTests/XcodeGenKitTests.swift
@@ -1,25 +1,31 @@
 import XCTest
 import PathKit
 import XcodeGenKit
+import xcodeproj
+
+let fixturePath = Path(#file).parent().parent().parent() + "Fixtures"
+
+func testGeneration(specPath: Path, projectPath: Path) throws {
+    let spec = try Spec(path: specPath)
+    let lintedSpec = SpecLinter.lint(spec)
+    if lintedSpec.errors.isEmpty {
+        let generator = ProjectGenerator(spec: lintedSpec.spec)
+        let project = try generator.generate()
+        try project.write(path: projectPath, override: true)
+        _ = try XcodeProj(path: projectPath)
+    } else {
+        XCTFail("Spec has errors:\n\(lintedSpec.errors.map { $0.description}.joined(separator: "\n"))")
+    }
+}
 
 class XcodeGenTests: XCTestCase {
 
-    let fixturePath = Path(#file).parent().parent().parent() + "Fixtures"
 
-    func testGeneration() throws {
-        let specPath = fixturePath + "TestProject/spec.yml"
-        let spec = try Spec(path: specPath)
-        let lintedSpec = SpecLinter.lint(spec)
-        if lintedSpec.errors.isEmpty {
-            let generator = ProjectGenerator(spec: lintedSpec.spec)
-            let project = try generator.generate()
-            try project.write(path: fixturePath + "TestProject/spec.xcodeproj", override: true)
-        } else {
-            XCTFail("Spec has errors:\n\(lintedSpec.errors.map { $0.description}.joined(separator: "\n"))")
-        }
+    func test_project_generation() throws {
+        try testGeneration(specPath: fixturePath + "TestProject/spec.yml", projectPath: fixturePath + "TestProject/spec.xcodeproj")
     }
 
     static var allTests = [
-        ("testGeneration", testGeneration),
+        ("testGeneration", test_project_generation),
         ]
 }
