6e8a153f62067d07b6b411b23d3d100fdd3c7f01
Yonas Kolb <yonaskolb@gmail.com>
2017-07-30 09:15:43 +0200

    update YAMS to 0.3

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/kylef/PathKit.git", majorVersion: 0, minor: 8),
         .Package(url: "https://github.com/kylef/Commander.git", majorVersion: 0, minor: 6),
-        .Package(url: "https://github.com/jpsim/Yams.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/jpsim/Yams.git", majorVersion: 0, minor: 3),
         .Package(url: "https://github.com/yonaskolb/JSONUtilities.git", majorVersion: 3, minor: 3),
         .Package(url: "https://github.com/kylef/Spectre.git", majorVersion: 0, minor: 7),
         .Package(url: "https://github.com/onevcat/Rainbow", majorVersion: 2),
