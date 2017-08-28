6bfb0eade62985645f90b90c6d456eab2058219c
Yonas Kolb <yonaskolb@gmail.com>
2017-08-28 17:05:50 +0200

    fix "" not escaping in build scripts

--- Sources/XcodeGenKit/PBXProjGenerator.swift
+++ Sources/XcodeGenKit/PBXProjGenerator.swift
@@ -293,13 +293,14 @@ public class PBXProjGenerator {
 
         func getBuildScript(buildScript: BuildScript) throws -> PBXShellScriptBuildPhase {
 
-            let shellScript: String
+            var shellScript: String
             switch buildScript.script {
             case let .path(path):
                 shellScript = try (basePath + path).read()
             case let .script(script):
                 shellScript = script
             }
+            shellScript = shellScript.replacingOccurrences(of: "\"", with: "\\\"") // TODO: remove when xcodeproj escaped values
             let shellScriptPhase = PBXShellScriptBuildPhase(
                 reference: generateUUID(PBXShellScriptBuildPhase.self, String(describing: buildScript.name) + shellScript + target.name),
                 files: [],
