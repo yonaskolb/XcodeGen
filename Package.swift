05e7928c7c74d61ef081cdbc8c71cd0b9d6bb39c
Yonas Kolb <yonaskolb@gmail.com>
2017-09-14 00:48:57 +0200

    upgrade to Swift 4

--- Package.swift
+++ Package.swift
@@ -1,21 +1,41 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
 
 import PackageDescription
 
 let package = Package(
     name: "XcodeGen",
-    targets: [
-        Target(name: "XcodeGen", dependencies: ["XcodeGenKit"]),
-        Target(name: "XcodeGenKit", dependencies: ["ProjectSpec"]),
-        Target(name: "ProjectSpec"),
+    products: [
+        .executable(name: "XcodeGen", targets: ["XcodeGen"]),
+        .library(name: "XcodeGenKit", targets: ["XcodeGenKit"]),
     ],
     dependencies: [
-        .Package(url: "https://github.com/kylef/PathKit.git", majorVersion: 0, minor: 8),
-        .Package(url: "https://github.com/kylef/Commander.git", majorVersion: 0, minor: 6),
-        .Package(url: "https://github.com/jpsim/Yams.git", majorVersion: 0, minor: 3),
-        .Package(url: "https://github.com/yonaskolb/JSONUtilities.git", majorVersion: 3, minor: 3),
-        .Package(url: "https://github.com/kylef/Spectre.git", majorVersion: 0, minor: 7),
-        .Package(url: "https://github.com/onevcat/Rainbow", majorVersion: 2),
-        .Package(url: "https://github.com/swift-xcode/xcodeproj.git", majorVersion: 0, minor: 1),
+        .package(url: "https://github.com/kylef/PathKit.git", from: "0.8.0"),
+        .package(url: "https://github.com/kylef/Commander.git", from: "0.6.1"),
+        .package(url: "https://github.com/jpsim/Yams.git", from: "0.3.6"),
+        .package(url: "https://github.com/yonaskolb/JSONUtilities.git", from: "3.3.0"),
+        .package(url: "https://github.com/kylef/Spectre.git", from: "0.7.0"),
+        .package(url: "https://github.com/onevcat/Rainbow.git", from: "2.1.0"),
+        .package(url: "https://github.com/swift-xcode/xcodeproj.git", .exact("0.1.1")),
+    ],
+    targets: [
+        .target(name: "XcodeGen", dependencies: [
+          "XcodeGenKit",
+          "Commander",
+          "Rainbow",
+        ]),
+        .target(name: "XcodeGenKit", dependencies: [
+          "ProjectSpec",
+          "JSONUtilities",
+          "xcodeproj",
+          "PathKit",
+        ]),
+        .target(name: "ProjectSpec", dependencies: [
+          "JSONUtilities",
+          "xcodeproj",
+          "Yams",
+        ]),
+        .testTarget(name: "XcodeGenKitTests", dependencies: [
+          "XcodeGenKit",
+        ])
     ]
 )
