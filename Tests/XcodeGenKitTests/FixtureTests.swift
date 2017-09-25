b1888dc62f073d54e92c80fde70280ed522600dc
Yonas Kolb <yonaskolb@gmail.com>
2017-09-25 16:12:33 +0200

    fail fixture test if project has changed

--- Tests/XcodeGenKitTests/FixtureTests.swift
+++ Tests/XcodeGenKitTests/FixtureTests.swift
@@ -10,8 +10,13 @@ func generate(specPath: Path, projectPath: Path) throws {
     let spec = try ProjectSpec(path: specPath)
     let generator = ProjectGenerator(spec: spec, path: specPath.parent())
     let project = try generator.generateProject()
+    let oldProject = try XcodeProj(path: projectPath)
     try project.write(path: projectPath, override: true)
-    _ = try XcodeProj(path: projectPath)
+
+    let newProject = try XcodeProj(path: projectPath)
+    if newProject != oldProject {
+        throw failure("\(projectPath.string) has changed. If change is legitimate commit the change and run test again")
+    }
 }
 
 func fixtureTests() {
