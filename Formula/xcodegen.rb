6385097041b29b62e0cd6fffd2ceebb396e7fc52
Yonas Kolb <yonaskolb@gmail.com>
2017-09-29 18:22:05 +0200

    update homebrew formula

--- Formula/xcodegen.rb
+++ Formula/xcodegen.rb
@@ -2,7 +2,7 @@ class Xcodegen < Formula
   desc "Tool that generates your Xcode project from a project spec"
   homepage "https://github.com/yonaskolb/XcodeGen"
   url "https://github.com/yonaskolb/XcodeGen/archive/1.2.3.tar.gz"
-  sha256 "ae956169a0f06ecaa2a3b11c8a66829112b4c076c397a02fad7185404d8baae9"
+  sha256 "d3566e58a0012edc8b4b49a2da5f8eda1ea231a2a2ce2da0751cc5a207213535"
   head "https://github.com/yonaskolb/XcodeGen.git"
 
   depends_on :xcode
