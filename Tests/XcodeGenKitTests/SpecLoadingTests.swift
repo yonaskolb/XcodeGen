91056efc8a61404460c6e111c81c69b7f9f8e7d6
rmalik <rmalik@pinterest.com>
2017-11-03 12:10:00 -0700

    Load json files directly with NSJSONSerialization if the spec path extension is `json`

--- Tests/XcodeGenKitTests/SpecLoadingTests.swift
+++ Tests/XcodeGenKitTests/SpecLoadingTests.swift
@@ -48,6 +48,25 @@ func specLoadingTests() {
         }
     }
 
+    describe("Spec Loader JSON") {
+        $0.it("merges includes") {
+            let path = fixturePath + "include_test.json"
+            let spec = try ProjectSpec(path: path)
+
+            try expect(spec.name) == "NewName"
+            try expect(spec.settingGroups) == [
+                "test": Settings(dictionary: ["MY_SETTING1": "NEW VALUE", "MY_SETTING2": "VALUE2", "MY_SETTING3": "VALUE3"]),
+                "new": Settings(dictionary: ["MY_SETTING": "VALUE"]),
+                "toReplace": Settings(dictionary: ["MY_SETTING2": "VALUE2"]),
+            ]
+            try expect(spec.targets) == [
+                Target(name: "IncludedTargetNew", type: .application, platform: .tvOS, sources: ["NewSource"]),
+                Target(name: "NewTarget", type: .application, platform: .iOS),
+            ]
+        }
+    }
+
+
     describe("Project Spec Parser") {
 
         $0.it("fails with incorrect platform") {
