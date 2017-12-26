da5993d07e751bc922069c00c23cfb5ad179f116
Yonas Kolb <yonaskolb@gmail.com>
2017-12-26 22:49:17 +0800

    update dependencies

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(name: "ProjectSpec", targets: ["ProjectSpec"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/kylef/PathKit.git", .revision("a667cd3")),
+        .package(url: "https://github.com/kylef/PathKit.git", from: "0.9.0"),
         .package(url: "https://github.com/kylef/Commander.git", from: "0.8.0"),
         .package(url: "https://github.com/jpsim/Yams.git", from: "0.3.6"),
         .package(url: "https://github.com/yonaskolb/JSONUtilities.git", from: "3.3.0"),
