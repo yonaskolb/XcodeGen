d07f160bae748df617318c21ab34e3274e15150d
toshi0383 <t.suzuki326@gmail.com>
2018-02-26 19:19:48 +0900

    Fix archive.sh and Makefile

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-TOOL_NAME=XcodeGen
-TOOL_NAME_LOWER=xcodegen
-TMP=$(mktemp -d)/$TOOL_NAME
+TMP=$(mktemp -d)/${TOOL_NAME:?}
 BINDIR=$TMP/bin
 SHAREDIR=$TMP/share
-ZIPFILE=$TMP/$TOOL_NAME_LOWER.zip
-INSTALLSH=scripts/install-binary.sh
+ZIPFILE=$TMP/${TOOL_NAME_LOWER:?}.zip
+INSTALLSH=scripts/install.sh
+
+# copy
 
 mkdir -p $BINDIR
 cp -f .build/release/$TOOL_NAME_LOWER $BINDIR
@@ -13,13 +13,18 @@ cp -f .build/release/$TOOL_NAME_LOWER $BINDIR
 mkdir -p $SHAREDIR
 cp -R SettingPresets $SHAREDIR/SettingPresets
 
-sed -e 's/^BASE_DIR=.*/BASE_DIR=$(cd `dirname $0`; pwd)/' -e '/.*[zZ][iI][pP].*/d' $INSTALLSH \
-    > $TMP/install.sh
-chmod +x $TMP/install.sh
+cp $INSTALLSH $TMP
+
+# zip
 
 (cd $TMP/..; zip -r $ZIPFILE $TOOL_NAME)
+
+# print sha
+
 SHA=$(cat $ZIPFILE | shasum -a 256 | sed 's/ .*//')
 echo "SHA: $SHA"
 mv $ZIPFILE .
+
+# cleanup
 
 rm -rf $TMP
