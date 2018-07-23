e915fdf4212d0038685e004685e70393e4003e15
Yonas Kolb <yonaskolb@gmail.com>
2018-07-23 22:17:22 +1000

    add sorting test

--- Tests/XcodeGenKitTests/SourceGeneratorTests.swift
+++ Tests/XcodeGenKitTests/SourceGeneratorTests.swift
@@ -478,6 +478,38 @@ class SourceGeneratorTests: XCTestCase {
                 let groups = try pbxProj.getMainGroup().children.compactMap { pbxProj.objects.getFileElement(reference: $0)?.nameOrPath }
                 try expect(groups) == ["A", "P", "S", "Frameworks", "Products"]
             }
+
+            $0.it("sorts files") {
+                let directories = """
+                    Sources:
+                    - file3.swift
+                    - file.swift
+                    - 10file.a
+                    - 1file.a
+                    - file2.swift
+                    - group2:
+                        - file.swift
+                    - group:
+                        - file.swift
+                """
+                try createDirectories(directories)
+
+                let target = Target(name: "Test", type: .application, platform: .iOS, sources: ["Sources"], dependencies: [Dependency(type: .carthage, reference: "Alamofire")])
+                let project = Project(basePath: directoryPath, name: "Test", targets: [target])
+
+                let pbxProj = try project.generatePbxProj()
+                let group = pbxProj.objects.group(named: "Sources", inGroup: try pbxProj.getMainGroup())!.object
+                let names = group.children.compactMap { pbxProj.objects.getFileElement(reference: $0)?.nameOrPath }
+                try expect(names) == [
+                    "group",
+                    "group2",
+                    "1file.a",
+                    "10file.a",
+                    "file.swift",
+                    "file2.swift",
+                    "file3.swift",
+                ]
+            }
         }
     }
 }
