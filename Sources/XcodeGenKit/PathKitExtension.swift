6933cded8b24ad6f12f328c5e381cd5406b5fd66
toshi0383 <t.suzuki326@gmail.com>
2018-09-23 22:37:01 +0900

    Add comment and improve code

--- Sources/XcodeGenKit/PathKitExtension.swift
+++ Sources/XcodeGenKit/PathKitExtension.swift
@@ -3,18 +3,22 @@ import PathKit
 
 extension Path {
 
-    /// Treat this as a resource instead of a normal directory.
+    /// Treat this as a file (resource or source) instead of a normal directory.
     var isNonFolderDirectory: Bool {
 
-        if isFile {
+        if isFile || self.extension == nil {
             return false
         }
 
         if let uti = try! URL(fileURLWithPath: self.string)
             .resourceValues(forKeys: [URLResourceKey.typeIdentifierKey])
             .typeIdentifier {
-            // NOTE: lproj is public.folder
+
+            // If uti is `public.folder` or `dyn*`, it's a normal directory in most cases.
+            // But for example *.lproj appears to be a `public.folder`.
+            // So make sure to filter to treat it as a special directory.
             return uti != "public.folder" && !uti.starts(with: "dyn")
+
         }
 
         return false
