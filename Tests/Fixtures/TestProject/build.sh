7f7b3d3d94fe536254a61f068100e13496a4170f
Yonas Kolb <yonaskolb@gmail.com>
2018-09-29 20:08:48 +1000

    add xcpretty to output

--- Tests/Fixtures/TestProject/build.sh
+++ Tests/Fixtures/TestProject/build.sh
@@ -2,4 +2,4 @@
 set -e
 
 carthage bootstrap --cache-builds
-xcodebuild -project Project.xcodeproj -scheme "App_iOS Test" -configuration "Test Debug" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
+xcodebuild -project Project.xcodeproj -scheme "App_iOS Test" -configuration "Test Debug" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO | xcpretty
