eb38020f9bd6f31d31d2085cdc4f2c9a6903312d
Yonas Kolb <yonaskolb@gmail.com>
2018-10-14 21:49:51 +1100

    update JSONUtilities

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .package(url: "https://github.com/kylef/PathKit.git", from: "0.9.0"),
         .package(url: "https://github.com/kylef/Commander.git", from: "0.8.0"),
         .package(url: "https://github.com/jpsim/Yams.git", from: "1.0.0"),
-        .package(url: "https://github.com/yonaskolb/JSONUtilities.git", from: "4.0.0"),
+        .package(url: "https://github.com/yonaskolb/JSONUtilities.git", from: "4.1.0"),
         .package(url: "https://github.com/kylef/Spectre.git", from: "0.9.0"),
         .package(url: "https://github.com/onevcat/Rainbow.git", from: "3.0.0"),
         .package(url: "https://github.com/tuist/xcodeproj.git", from: "6.2.0"),
