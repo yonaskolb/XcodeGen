1d62b46423e1ec021a02bf64f0b03395e26d6490
Ell Neal <elliot@emdentec.com>
2019-01-27 12:46:00 +0000

    #whitespace

--- Sources/ProjectSpec/SpecLoader.swift
+++ Sources/ProjectSpec/SpecLoader.swift
@@ -4,7 +4,7 @@ import PathKit
 import Yams
 
 extension Project {
-    
+
     public init(path: Path) throws {
         let basePath = path.parent()
         let template = try Spec(filename: path.lastComponent, basePath: basePath)
