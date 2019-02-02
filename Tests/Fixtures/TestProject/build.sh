474874d00fe6d5a8c0c5a06cc567dd53433e2f5b
Yonas Kolb <yonaskolb@gmail.com>
2019-02-02 13:23:04 +1100

    add --quite to xcodebuild

--- Tests/Fixtures/TestProject/build.sh
+++ Tests/Fixtures/TestProject/build.sh
@@ -4,7 +4,7 @@ set -e
 carthage bootstrap --cache-builds
 echo "
 Building iOS app"
-xcodebuild -project Project.xcodeproj -scheme "App_iOS Test" -configuration "Test Debug" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
+xcodebuild -quiet -project Project.xcodeproj -scheme "App_iOS Test" -configuration "Test Debug" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
 echo "
 Building macOS app"
-xcodebuild -project Project.xcodeproj -scheme "App_macOS" -configuration "Test Debug" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
+xcodebuild -quiet -project Project.xcodeproj -scheme "App_macOS" -configuration "Test Debug" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
