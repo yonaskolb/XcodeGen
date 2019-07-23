9eb9638216a6017b18806f6adab675ae16195b95
Beniamin Sarkisyan <bsarkisyan@projectx019.com>
2019-07-23 12:13:45 +0300

    Fix merge issues

--- Sources/XcodeGenKit/PBXProjGenerator.swift
+++ Sources/XcodeGenKit/PBXProjGenerator.swift
@@ -393,10 +393,13 @@ public class PBXProjGenerator {
                     if sortOrder1 != sortOrder2 {
                         return sortOrder1 < sortOrder2
                     } else {
-                        return child1.context ?? "" < child2.context ?? ""
+                        if child1.nameOrPath != child2.nameOrPath {
+                            return child1.nameOrPath.localizedStandardCompare(child2.nameOrPath) == .orderedAscending
+                        } else {
+                            return child1.context ?? "" < child2.context ?? ""
+                        }
                     }
-                }
-        
+            }
             group.children = children.filter { $0 != group }
         }
     
