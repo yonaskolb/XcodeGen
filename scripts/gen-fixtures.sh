f016321bda4e9252ff0f8af2c1da563320a41890
Bradley Smith <bradleysmith@wayfair.com>
2019-08-02 14:38:12 -0400

    Update generate fixtures script

--- scripts/gen-fixtures.sh
+++ scripts/gen-fixtures.sh
@@ -3,3 +3,4 @@ set -e
 
 swift run xcodegen --spec Tests/Fixtures/TestProject/project.yml
 swift run xcodegen --spec Tests/Fixtures/CarthageProject/project.yml
+swift run xcodegen --spec Tests/Fixtures/TestProject2/project.yml
