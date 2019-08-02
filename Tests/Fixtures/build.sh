f43df8fb4c581345ae875ea7f277b2c2ee2f824b
Bradley Smith <bradleysmith@wayfair.com>
2019-08-02 14:56:09 -0400

    Update build scripts to support workspace

---
+++ Tests/Fixtures/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+carthage bootstrap --cache-builds
+echo "
+⚙️ Building iOS app"
+xcodebuild -quiet -workspace TestWorkspace.xcworkspace -scheme "App_iOS Test" -configuration "Test Debug" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGN_ENTITLEMENTS="" CODE_SIGNING_ALLOWED="NO"
+echo "✅ Successfully built iOS app"
+
+echo "
+⚙️ Building macOS app"
+xcodebuild -quiet -project TestProject/Project.xcodeproj -scheme "App_macOS" -configuration "Test Debug" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGN_ENTITLEMENTS="" CODE_SIGNING_ALLOWED="NO"
+echo "✅ Successfully built macOS app"
--- scripts/build-fixtures.sh
+++ scripts/build-fixtures.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-cd Tests/Fixtures/TestProject
+cd Tests/Fixtures
 ./build.sh
