6c655c177f39d06f3c51c7be67c762794a0c010e
Bradley Smith <bradley.d.smith11@gmail.com>
2019-09-08 10:46:16 -0400

    Update fixture scripts

--- scripts/gen-fixtures.sh
+++ scripts/gen-fixtures.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-swift run xcodegen --spec Tests/Fixtures/TestProject/project.yml
-swift run xcodegen --spec Tests/Fixtures/CarthageProject/project.yml
-swift run xcodegen --spec Tests/Fixtures/TestProject2/project.yml
+swift run xcodegen --spec ../Tests/Fixtures/TestProject/project.yml
+swift run xcodegen --spec ../Tests/Fixtures/CarthageProject/project.yml
+swift run xcodegen --spec ../Tests/Fixtures/TestProject2/project.yml
