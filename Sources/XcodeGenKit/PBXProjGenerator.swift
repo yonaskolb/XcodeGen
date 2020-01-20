3a9131a21a835fdc6d16d1e3e58f0ec5e6c16111
sipao <40229310+sipao@users.noreply.github.com>
2020-01-21 06:47:31 +0900

    Add support On Demand Resources (#753)
    * added on-demand-resources setting to project.yml
    
    * update documents
    
    * Apply suggestions from code review
    
    fixed docs
    
    Co-Authored-By: Yonas Kolb <yonaskolb@users.noreply.github.com>
    
    * Make it possible to resourceTags convert to json
    
    * Use sorted() instead of Array initializer
    
    * Avoid merge assetTags if it is empty
    
    * Changed access to resourceTags to inline
    
    * Don't add resourceTags other than chosenBuildPhase is .resources
    
    * update CHANGELOG.md
    
    * update spec loading test
    
    * add a new "generates resource tags" test case
    
    * add resourceTags in Test Fixture and update
    
    Co-authored-by: Yonas Kolb <yonaskolb@users.noreply.github.com>

--- Sources/XcodeGenKit/PBXProjGenerator.swift
+++ Sources/XcodeGenKit/PBXProjGenerator.swift
@@ -233,9 +233,19 @@ public class PBXProjGenerator {
             .sorted { $0.nameOrPath.localizedStandardCompare($1.nameOrPath) == .orderedAscending }
             .map { $0 }
 
-        let projectAttributes: [String: Any] = ["LastUpgradeCheck": project.xcodeVersion]
+        let assetTags = Set(project.targets
+            .map { target in
+                target.sources.map { $0.resourceTags }.flatMap { $0 }
+            }.flatMap { $0 }
+        ).sorted()
+        
+        var projectAttributes: [String: Any] = ["LastUpgradeCheck": project.xcodeVersion]
             .merged(project.attributes)
-
+        
+        if !assetTags.isEmpty {
+            projectAttributes["knownAssetTags"] = assetTags
+        }
+        
         let knownRegions = sourceGenerator.knownRegions
         pbxProject.knownRegions = (knownRegions.isEmpty ? ["en"] : knownRegions).union(["Base"]).sorted()
         pbxProject.packages = packageReferences.sorted { $0.key < $1.key }.map { $1 }
