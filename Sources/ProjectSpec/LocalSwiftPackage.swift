47c17d65eee937f86f4d37b74dd92716edf1e723
freddi <bohemianyuki@gmail.com>
2020-03-03 22:24:56 +0900

    Fixed test by defining initializer of LocalSwiftPackage

--- Sources/ProjectSpec/LocalSwiftPackage.swift
+++ Sources/ProjectSpec/LocalSwiftPackage.swift
@@ -5,10 +5,23 @@ import Version
 
 public struct LocalSwiftPackage: Equatable {
     public let path: String
+    
+    public init(path: String) {
+        self.path = path
+    }
 }
 
 extension LocalSwiftPackage: JSONObjectConvertible {
     public init(jsonDictionary: JSONDictionary) throws {
         path = try jsonDictionary.json(atKeyPath: "path")
+    }
+}
+
+extension LocalSwiftPackage: JSONEncodable {
+    public func toJSONValue() -> Any {
+        var dictionary: JSONDictionary = [:]
+        dictionary["path"] = path
+        
+        return dictionary
     }
 }
