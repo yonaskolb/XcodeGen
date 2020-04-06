45744eae85b98eb0705f60c0f7975b163461b924
Sebastian Celis <sebastian@sebastiancelis.com>
2020-04-06 08:10:30 -0500

    Allow messages applications to skip the compile sources phase
    By default, simple iOS sticker packs created in Xcode do not include
    a compile sources phase. This change will allow messages applications
    with an empty list of sources to skip that phase entirely.

--- Sources/ProjectSpec/XCProjExtensions.swift
+++ Sources/ProjectSpec/XCProjExtensions.swift
@@ -41,6 +41,17 @@ extension PBXProductType {
     public var name: String {
         rawValue.replacingOccurrences(of: "com.apple.product-type.", with: "")
     }
+
+    public var canSkipCompileSourcesBuildPhase: Bool {
+        switch self {
+        case .stickerPack, .messagesApplication:
+            // Sticker packs and simple messages applications without sources should not include a
+            // compile sources build phase. Doing so can cause Xcode to produce an error on build.
+            return true
+        default:
+            return false
+        }
+    }
 }
 
 extension Platform {
