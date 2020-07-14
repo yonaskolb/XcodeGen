139b61a6e5c1d9bdfa4e35b11e584419f9e5c62f
Brentley Jones <github@brentleyjones.com>
2020-07-14 08:10:24 -0500

    Add support for App Clips
    Embeds App Clips into the containing app when they are a dependency.

---
+++ Tests/Fixtures/TestProject/App_Clip/ViewController.swift
@@ -0,0 +1,15 @@
+import Contacts
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        _ = CNContact()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
