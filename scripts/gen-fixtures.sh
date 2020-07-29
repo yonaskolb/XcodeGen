ab1a6cb425d7c17c41f4f9d0b4fa9c9329b4c16f
Dan Fleming <dflems@spotify.com>
2020-07-29 12:49:01 -0400

    Patch #909 to not fail CI (#917)

--- scripts/gen-fixtures.sh
+++ scripts/gen-fixtures.sh
@@ -3,5 +3,6 @@ set -e
 
 swift run xcodegen --spec Tests/Fixtures/TestProject/AnotherProject/project.yml
 swift run xcodegen --spec Tests/Fixtures/TestProject/project.yml
+swift run xcodegen --spec Tests/Fixtures/TestProject/project-xcode-12.yml
 swift run xcodegen --spec Tests/Fixtures/CarthageProject/project.yml
 swift run xcodegen --spec Tests/Fixtures/SPM/project.yml
