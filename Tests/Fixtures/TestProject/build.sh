c0d2d948f123e8e9bad22eb0fc9181ad101af4f9
Dan Fleming <dflems@spotify.com>
2020-08-18 12:06:58 -0400

    fixtures: add workarounds for Xcode 12

--- Tests/Fixtures/TestProject/build.sh
+++ Tests/Fixtures/TestProject/build.sh
@@ -1,33 +1,30 @@
 #!/bin/bash
 set -e
 
+XCODE_VERSION=$(/usr/libexec/PlistBuddy -c "Print :DTXcode" "$(xcode-select -p)/../Info.plist")
+
 CARTHAGE_DYNAMIC_FRAMEWORKS=(Result)
 CARTHAGE_STATIC_FRAMEWORKS=(SwiftyJSON swift-nonempty)
 
-carthage bootstrap $CARTHAGE_DYNAMIC_FRAMEWORKS --cache-builds
+XCODE_XCCONFIG_FILE="$PWD/carthage_dynamic.xcconfig" \
+    carthage bootstrap $CARTHAGE_DYNAMIC_FRAMEWORKS --cache-builds
 
-# Prepare xcconfig for static bootstrapping
-STATIC_CONFIG=$(mktemp -d)/static.xcconfig
-echo "MACH_O_TYPE = staticlib" > $STATIC_CONFIG
-
-XCODE_XCCONFIG_FILE=$STATIC_CONFIG \
+XCODE_XCCONFIG_FILE="$PWD/carthage_static.xcconfig" \
     carthage bootstrap $CARTHAGE_STATIC_FRAMEWORKS --cache-builds
-
-XCODE_VERSION=$(/usr/libexec/PlistBuddy -c "Print :DTXcode" "$(xcode-select -p)/../Info.plist")
 
 echo "
 ⚙️ Building iOS app"
-xcodebuild -quiet -workspace Workspace.xcworkspace -scheme "App_iOS Test" -configuration "Test Debug" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGN_ENTITLEMENTS="" CODE_SIGNING_ALLOWED="NO"
+xcodebuild -quiet -workspace Workspace.xcworkspace -scheme "App_iOS Test" -configuration "Test Debug" -xcconfig fixtures.xcconfig
 echo "✅ Successfully built iOS app"
 
 if [[ "$XCODE_VERSION" == 12* ]]; then
     echo "
     ⚙️ Building iOS app (Xcode 12+)"
-    xcodebuild -quiet -project ProjectXcode12.xcodeproj -scheme "App_iOS_With_Clip Test" -configuration "Test Debug" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGN_ENTITLEMENTS="" CODE_SIGNING_ALLOWED="NO"
+    xcodebuild -quiet -project ProjectXcode12.xcodeproj -scheme "App_iOS_With_Clip Test" -configuration "Test Debug" -xcconfig fixtures.xcconfig
     echo "✅ Successfully built iOS app (Xcode 12+)"
 fi
 
 echo "
 ⚙️ Building macOS app"
-xcodebuild -quiet -workspace Workspace.xcworkspace -scheme "App_macOS" -configuration "Test Debug" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGN_ENTITLEMENTS="" CODE_SIGNING_ALLOWED="NO"
+xcodebuild -quiet -workspace Workspace.xcworkspace -scheme "App_macOS" -configuration "Test Debug" -xcconfig fixtures.xcconfig
 echo "✅ Successfully built macOS app"
