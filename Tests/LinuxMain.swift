5dcf89cf1663fbcf38068675f5ab27b40655cac7
Elliott Williams <emw@yelp.com>
2020-11-23 15:52:29 -0800

    fatalError when --enable-test-discovery is not used

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,17 +1,3 @@
-import XCTest
-
-import CoreTests
-import FixtureTests
-import PerformanceTests
-import ProjectSpecTests
-import XcodeGenKitTests
-
-var tests = [XCTestCaseEntry]()
-tests += CoreTests.__allTests()
-tests += FixtureTests.__allTests()
-tests += PerformanceTests.__allTests()
-tests += ProjectSpecTests.__allTests()
-tests += XcodeGenKitTests.__allTests()
-
-XCTMain(tests)
+// LinuxMain.swift
+fatalError("Run the tests with `swift test --enable-test-discovery`.")
 
