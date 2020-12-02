a742f6fe42e86e475bc5c4021e75f82ecc87602e
Jierong Li <ihsan.lee@gmail.com>
2020-12-02 18:30:30 +0900

    Add baseOnDependencyAnalysis to BuildScript

--- Sources/XcodeGenKit/PBXProjGenerator.swift
+++ Sources/XcodeGenKit/PBXProjGenerator.swift
@@ -478,7 +478,8 @@ public class PBXProjGenerator {
             shellPath: buildScript.shell ?? "/bin/sh",
             shellScript: shellScript,
             runOnlyForDeploymentPostprocessing: buildScript.runOnlyWhenInstalling,
-            showEnvVarsInLog: buildScript.showEnvVars
+            showEnvVarsInLog: buildScript.showEnvVars,
+            alwaysOutOfDate: !buildScript.baseOnDependencyAnalysis
         )
         return addObject(shellScriptPhase)
     }
