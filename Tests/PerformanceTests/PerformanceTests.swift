e7f753785e6ee135e5ea77268d149e9cd10bb7ed
Mathieu Olivari <1377279+ma-oli@users.noreply.github.com>
2022-11-03 01:05:46 -0700

    Fix includes related issues and improve their performances (#1275)
    * Fix recursive include path when relativePath is not set
    
    If relativePath is not set on a particular include, the first level of
    include will currently work, but starting at the second level of
    iteration, the computed include path will fail as relativePath will be
    appended over and over onto the filePath. We're fixing that recursion
    problem here and adding the corresponding tests to make sure it doesn't
    happen again.
    
    * Include projectRoot in include paths
    
    The projectRoot setting (when specified) is currently ignored when
    computing the include paths. We're fixing that in that commit.
    
    * Use memoization during recursive SpecFiles creation
    
    SpecFile objects are created by recursive through includes. On a large
    project with programatically generated SpecFile, it is not rare to have
    hundreds of SpecFiles, creating a large web of include dependencies.
    In such a case, it is not rare either for a particular SpecFile to be
    included by multiple other SpecFiles. When that happens, XcodeGen
    currently creates a SpecFile object every time a SpecFile gets included,
    which can lead to an exponential growth of includes.
    
    I have seen hundreds of files being turned into hundred of thousands of
    SpecFile object creations, which leads to an impractical XcodeGen run of
    tens of minutes.
    
    This change adds memoization during SpecFile recursion, in order to
    reuse the previously created SpecFiles, if available, instead of
    re-creating them.
    
    * Update CHANGELOG.md
    
    Add the following changes to the changelog:
    * b97bdc4 - Use memoization during recursive SpecFiles creation
    * a6b96ad - Include projectRoot in include paths
    * 557b074 - Fix recursive include path when relativePath is not set

--- Tests/PerformanceTests/PerformanceTests.swift
+++ Tests/PerformanceTests/PerformanceTests.swift
@@ -13,9 +13,12 @@ class GeneratedPerformanceTests: XCTestCase {
         let project = try Project.testProject(basePath: basePath)
         let specPath = basePath + "project.yaml"
         try dumpYamlDictionary(project.toJSONDictionary(), path: specPath)
+        var cachedSpecFiles: [Path: SpecFile] = [:]
 
         measure {
-            let spec = try! SpecFile(path: specPath, variables: ProcessInfo.processInfo.environment)
+            let spec = try! SpecFile(path: specPath,
+                                     cachedSpecFiles: &cachedSpecFiles,
+                                     variables: ProcessInfo.processInfo.environment)
             _ = spec.resolvedDictionary()
         }
     }
