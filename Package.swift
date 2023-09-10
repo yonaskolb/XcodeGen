5fa61afdbc7b53dd15ffd65cdee967e30ac2b6bf
freddi <freddi@linecorp.com>
2023-09-10 21:09:47 +0900

    update ArtifactBundleGen

--- Package.swift
+++ Package.swift
@@ -2,13 +2,6 @@
 
 import PackageDescription
 
-let macOSOnlyDependency: [Package.Dependency]
-#if !os(Linux)
-    macOSOnlyDependency = [Package.Dependency.package(url: "https://github.com/freddi-kit/ArtifactBundleGen", exact: "0.0.4")]
-#else
-    macOSOnlyDependency = []
-#endif
-
 let package = Package(
     name: "XcodeGen",
     platforms: [.macOS(.v10_13)],
@@ -27,7 +20,8 @@ let package = Package(
         .package(url: "https://github.com/jakeheis/SwiftCLI.git", from: "6.0.3"),
         .package(url: "https://github.com/mxcl/Version", from: "2.0.0"),
         .package(url: "https://github.com/SwiftDocOrg/GraphViz.git", exact: "0.2.0"),
-    ] + macOSOnlyDependency,
+        .package(url: "https://github.com/freddi-kit/ArtifactBundleGen", exact: "0.0.5")
+    ],
     targets: [
         .executableTarget(name: "XcodeGen", dependencies: [
             "XcodeGenCLI",
