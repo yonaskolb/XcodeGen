9e8343b1a61b81188da4520108bdfc25628ad620
Yonas Kolb <yonaskolb@users.noreply.github.com>
2025-04-05 14:46:41 +1100

    fix ci (#1537)
    * remove carthage from building test project
    
    * get test fixture compiling

--- Tests/Fixtures/TestProject/build.sh
+++ Tests/Fixtures/TestProject/build.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 set -e
 
-XCODE_VERSION=$(/usr/libexec/PlistBuddy -c "Print :DTXcode" "$(xcode-select -p)/../Info.plist")
-
-CARTHAGE_DYNAMIC_FRAMEWORKS=(Result)
-CARTHAGE_STATIC_FRAMEWORKS=(SwiftyJSON swift-nonempty)
-
-XCODE_XCCONFIG_FILE="$PWD/carthage_dynamic.xcconfig" \
-    carthage bootstrap $CARTHAGE_DYNAMIC_FRAMEWORKS --cache-builds
-
-XCODE_XCCONFIG_FILE="$PWD/carthage_static.xcconfig" \
-    carthage bootstrap $CARTHAGE_STATIC_FRAMEWORKS --cache-builds
-
 echo "
 ⚙️ Building iOS app"
 xcodebuild -quiet -workspace Workspace.xcworkspace -scheme "App_iOS Test" -configuration "Test Debug" -xcconfig fixtures.xcconfig
