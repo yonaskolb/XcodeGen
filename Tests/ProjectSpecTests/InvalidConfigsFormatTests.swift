58cdf9b2e6834fb891ee7a490c75660fe254cc19
Ryu0118 <ryunosuke.shibuya.3d@gmail.com>
2025-05-19 19:04:55 +0900

    Add tests for invalid settings.configs value formats

---
+++ Tests/ProjectSpecTests/InvalidConfigsFormatTests.swift
@@ -0,0 +1,35 @@
+import ProjectSpec
+import Testing
+import TestSupport
+import PathKit
+
+struct InvalidConfigsFormatTests {
+    @Test("throws invalidConfigsFormat error for non-dictionary configs entries")
+    func testNonDictionaryConfigsEntries() throws {
+        let path = fixturePath + "invalid_configs_value_non_dict.yml"
+        let expectedError = SpecParsingError.invalidConfigsFormat(keys: ["invalid_key0", "invalid_key1"])
+
+        #expect(throws: EquatableErrorBox(expectedError)) {
+            try perform(path: path)
+        }
+    }
+
+    private func perform(path: Path) throws {
+        do {
+            _ = try Project(path: path)
+        } catch let error as SpecParsingError {
+            throw EquatableErrorBox(error)
+        } catch {
+            throw error
+        }
+    }
+
+    // SpecParsingError does not conform to Equatable, so we wrap its description here
+    private struct EquatableErrorBox: Error, Equatable {
+        let description: String
+
+        init<E: Error & CustomStringConvertible>(_ error: E) {
+            self.description = error.description
+        }
+    }
+}
