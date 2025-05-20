c56b0724dc930e13f77e6ab9781ca593c272133f
Ryu0118 <ryunosuke.shibuya.3d@gmail.com>
2025-05-20 14:46:23 +0900

    Use old testing api and remove EquatableErrorBox

--- Tests/ProjectSpecTests/InvalidConfigsFormatTests.swift
+++ Tests/ProjectSpecTests/InvalidConfigsFormatTests.swift
@@ -9,27 +9,10 @@ struct invalidConfigsMappingFormatTests {
         let path = fixturePath + "invalid_configs_value_non_mapping.yml"
         let expectedError = SpecParsingError.invalidConfigsMappingFormat(keys: ["invalid_key0", "invalid_key1"])
 
-        #expect(throws: EquatableErrorBox(expectedError)) {
-            try perform(path: path)
-        }
-    }
-
-    private func perform(path: Path) throws {
-        do {
-            _ = try Project(path: path)
-        } catch let error as SpecParsingError {
-            throw EquatableErrorBox(error)
-        } catch {
-            throw error
-        }
-    }
-
-    // SpecParsingError does not conform to Equatable, so we wrap its description here
-    private struct EquatableErrorBox: Error, Equatable {
-        let description: String
-
-        init<E: Error & CustomStringConvertible>(_ error: E) {
-            self.description = error.description
+        #expect {
+            try Project(path: path)
+        } throws: { actualError in
+            (actualError as any CustomStringConvertible).description == expectedError.description
         }
     }
 }
