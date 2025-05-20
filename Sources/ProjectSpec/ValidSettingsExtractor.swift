9dc840575cfed5139c33f3c6a356bd51605bcede
Ryu0118 <ryunosuke.shibuya.3d@gmail.com>
2025-05-20 18:25:09 +0900

    Add document comments for ValidSettingsExtractor

--- Sources/ProjectSpec/ValidSettingsExtractor.swift
+++ Sources/ProjectSpec/ValidSettingsExtractor.swift
@@ -1,9 +1,13 @@
 import Foundation
 import JSONUtilities
 
+/// A helper for extracting and validating the `Settings` object from a JSON dictionary.
 struct ValidSettingsExtractor {
     let jsonDictionary: JSONDictionary
 
+    /// Attempts to extract and parse the `Settings` from the dictionary.
+    ///
+    /// - Returns: A valid `Settings` object
     func extract() throws -> Settings {
         do {
             return try jsonDictionary.json(atKeyPath: "settings")
