2b729a745b225602ca430382367689e84bf30504
Roman Podymov <podymfrombryansk@yandex.com>
2025-07-17 19:17:35 +0200

    func json(atKeyPath keyPath: String) -> String?

--- Sources/ProjectSpec/SwiftPackage.swift
+++ Sources/ProjectSpec/SwiftPackage.swift
@@ -105,66 +105,41 @@ extension SwiftPackage: JSONEncodable {
 extension SwiftPackage.VersionRequirement: JSONUtilities.JSONObjectConvertible {
 
     public init(jsonDictionary: JSONDictionary) throws {
-        if jsonDictionary["exactVersion"] != nil {
-            do {
-                let exactVersion: String = try jsonDictionary.json(atKeyPath: "exactVersion")
-                self = .exact(exactVersion)
-            } catch {
-                let exactVersion: Double = try jsonDictionary.json(atKeyPath: "exactVersion")
-                self = .exact(String(exactVersion))
+        func json(atKeyPath keyPath: String) -> String? {
+            if jsonDictionary[keyPath] != nil {
+                do {
+                    let value: String = try jsonDictionary.json(atKeyPath: .init(rawValue: keyPath))
+                    return value
+                } catch {
+                    do {
+                        let value: Double = try jsonDictionary.json(atKeyPath: .init(rawValue: keyPath))
+                        return String(value)
+                    } catch {
+                        return nil
+                    }
+                }
             }
-        } else if jsonDictionary["version"] != nil {
-            do {
-                let version: String = try jsonDictionary.json(atKeyPath: "version")
-                self = .exact(version)
-            } catch {
-                let version: Double = try jsonDictionary.json(atKeyPath: "version")
-                self = .exact(String(version))
-            }
-        } else if jsonDictionary["revision"] != nil {
-            do {
-                let revision: String = try jsonDictionary.json(atKeyPath: "revision")
-                self = .exact(revision)
-            } catch {
-                let revision: Double = try jsonDictionary.json(atKeyPath: "revision")
-                self = .exact(String(revision))
-            }
-        } else if jsonDictionary["branch"] != nil {
-            do {
-                let branch: String = try jsonDictionary.json(atKeyPath: "branch")
-                self = .exact(branch)
-            } catch {
-                let branch: Double = try jsonDictionary.json(atKeyPath: "branch")
-                self = .exact(String(branch))
-            }
+            return nil
+        }
+        
+        if let exactVersion = json(atKeyPath: "exactVersion") {
+            self = .exact(exactVersion)
+        } else if let version = json(atKeyPath: "version") {
+            self = .exact(version)
+        } else if let revision = json(atKeyPath: "revision") {
+            self = .revision(revision)
+        } else if let branch = json(atKeyPath: "branch") {
+            self = .branch(branch)
         } else if jsonDictionary["minVersion"] != nil && jsonDictionary["maxVersion"] != nil {
             let minimum: String = try jsonDictionary.json(atKeyPath: "minVersion")
             let maximum: String = try jsonDictionary.json(atKeyPath: "maxVersion")
             self = .range(from: minimum, to: maximum)
-        } else if jsonDictionary["minorVersion"] != nil {
-            do {
-                let minorVersion: String = try jsonDictionary.json(atKeyPath: "minorVersion")
-                self = .exact(minorVersion)
-            } catch {
-                let minorVersion: Double = try jsonDictionary.json(atKeyPath: "minorVersion")
-                self = .exact(String(minorVersion))
-            }
-        } else if jsonDictionary["majorVersion"] != nil {
-            do {
-                let majorVersion: String = try jsonDictionary.json(atKeyPath: "majorVersion")
-                self = .exact(majorVersion)
-            } catch {
-                let majorVersion: Double = try jsonDictionary.json(atKeyPath: "majorVersion")
-                self = .exact(String(majorVersion))
-            }
-        } else if jsonDictionary["from"] != nil {
-            do {
-                let from: String = try jsonDictionary.json(atKeyPath: "from")
-                self = .exact(from)
-            } catch {
-                let from: Double = try jsonDictionary.json(atKeyPath: "from")
-                self = .exact(String(from))
-            }
+        } else if let minorVersion = json(atKeyPath: "minorVersion") {
+            self = .upToNextMinorVersion(minorVersion)
+        } else if let majorVersion = json(atKeyPath: "majorVersion") {
+            self = .upToNextMajorVersion(majorVersion)
+        } else if let from = json(atKeyPath: "from") {
+            self = .upToNextMajorVersion(from)
         } else {
             throw SpecParsingError.unknownPackageRequirement(jsonDictionary)
         }
