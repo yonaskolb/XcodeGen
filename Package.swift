4e258da4e14416963fc5fd94b255aaf3c032a309
George Navarro <george@navarro.cc>
2025-07-24 08:41:26 -0700

    Update ArtifactBundleGen to 0.0.8

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         .package(url: "https://github.com/tuist/XcodeProj.git", exact: "8.27.7"),
         .package(url: "https://github.com/jakeheis/SwiftCLI.git", from: "6.0.3"),
         .package(url: "https://github.com/mxcl/Version", from: "2.0.0"),
-        .package(url: "https://github.com/freddi-kit/ArtifactBundleGen", exact: "0.0.6")
+        .package(url: "https://github.com/freddi-kit/ArtifactBundleGen", exact: "0.0.8")
     ],
     targets: [
         .executableTarget(name: "XcodeGen", dependencies: [
